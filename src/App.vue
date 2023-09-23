46b9fb9acfc47f824a3c1dcc6e35a5c3abbcce6e
Furkan <furkan.selim1999@hotmail.com>
2023-09-23 15:15:24 +0200

    - UI Changes

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,12 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
+  <header class="navbar">
     <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/news">News</RouterLink>
+      <nav class="nav-links">
+        <RouterLink to="/" class="nav-link">Home</RouterLink>
+        <RouterLink to="/about" class="nav-link">About</RouterLink>
+        <RouterLink to="/news" class="nav-link">News</RouterLink>
       </nav>
     </div>
   </header>
@@ -18,5 +18,38 @@ import HelloWorld from './components/HelloWorld.vue'
 </template>
 
 <style scoped>
+/* Add scoped attribute to ensure the styles only apply to this component */
 
+.navbar {
+  background-color: #007bff;
+  color: #fff;
+  padding: 10px 0;
+  margin-bottom: 10px;
+}
+
+.wrapper {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 0 20px;
+}
+
+.nav-links {
+  display: flex;
+  justify-content: space-around;
+  list-style: none;
+  padding: 0;
+}
+
+.nav-link {
+  text-decoration: none;
+  color: #fff;
+  font-size: 18px;
+  margin: 0 15px;
+  transition: color 0.3s ease-in-out;
+}
+
+.nav-link:hover {
+  color: #ffcc00; /* Change the color on hover */
+}
 </style>
+
